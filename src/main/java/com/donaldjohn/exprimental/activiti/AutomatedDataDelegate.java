8e271a41ac83521543ff0f53412e682abf3a2f3d
donaldjohn <donaldjohn@qq.com>
2018-12-13 07:57:57 +0800

    Init

---
+++ src/main/java/com/donaldjohn/exprimental/activiti/AutomatedDataDelegate.java
@@ -0,0 +1,19 @@
+package com.donaldjohn.exprimental.activiti;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+import java.util.Date;
+
+public class AutomatedDataDelegate implements JavaDelegate
+{
+    public void execute(DelegateExecution execution)
+    {
+        Date now = new Date();
+
+        execution.setVariable("autoWelcomeTime", now);
+
+        System.out.println("Faux call to backend for ["
+                + execution.getVariable("fullName") + "]");
+    }
+}
